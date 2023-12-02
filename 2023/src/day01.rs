c1cb815f3f77490b5292ed9d7492df584aa384fc
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2023-12-02 15:45:14 +0200

    2023: Finish day 1, add day 2

--- 2023/src/day01.rs
+++ 2023/src/day01.rs
@@ -1,9 +1,33 @@
 fn part1(input: String) -> String {
-    todo!()
+    input.lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            line.chars()
+                .filter_map(|c| c.to_digit(10))
+                .collect::<Vec<u32>>()
+        })
+        .map(|vec| vec.first().unwrap() * 10 + vec.last().unwrap())
+        .sum::<u32>()
+        .to_string()
 }
 
 fn part2(input: String) -> String {
-    todo!()
+    let modified = input.lines()
+        .map(|line| {
+            line.to_string()
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+    part1(modified)
 }
 
 fn main() {
