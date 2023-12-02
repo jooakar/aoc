94b7ed40f4def8c6510565a98fec89ff1b0f3fdc
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2023-12-02 20:18:27 +0200

    2022: Add initial files

---
+++ 2022/src/bin/day01.rs
@@ -0,0 +1,13 @@
+fn part1(input: String) -> String {
+    todo!()
+}
+
+fn part2(input: String) -> String {
+    todo!()
+}
+
+fn main() {
+    let input: String = include_str!("../../input/01").to_string();
+    println!("PART 1: {}", part1(input.clone()));
+    println!("PART 2: {}", part2(input));
+}
