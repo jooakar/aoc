6f732992cd553ff50bb5cdf6a133ecb2620f9664
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2023-12-02 21:02:13 +0200

    2022: Finish day 1

--- 2022/src/bin/day01.rs
+++ 2022/src/bin/day01.rs
@@ -1,9 +1,30 @@
+use std::cmp::max;
+
 fn part1(input: String) -> String {
-    todo!()
+    let mut res: u32 = 0;
+    for block in input.split("\n\n") {
+        let calories = block
+            .lines()
+            .map(|line| line.parse::<u32>().expect("parse error"))
+            .sum();
+        res = max(calories, res);
+    }
+
+    res.to_string()
 }
 
 fn part2(input: String) -> String {
-    todo!()
+    let mut res: [u32; 3] = [0; 3];
+    for block in input.split("\n\n") {
+        let calories = block
+            .lines()
+            .map(|line| line.parse::<u32>().expect("parse error"))
+            .sum();
+        let smallest = res.iter().enumerate().map(|(x,y)| (y,x)).min().unwrap().1;
+        if res[smallest] < calories { res[smallest] = calories }
+    }
+
+    res.into_iter().sum::<u32>().to_string()
 }
 
 fn main() {
