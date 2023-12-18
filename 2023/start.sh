6d87ef9c92a1ca667306c43c99d6cbf6350a5df8
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2023-12-18 19:58:44 +0200

    2023: Improve template

--- 2023/start.sh
+++ 2023/start.sh
@@ -6,6 +6,10 @@ FILENAME="src/bin/day$DATE.rs"
 if [ ! -f "$FILENAME" ]; then
     sed "s/DAYNUM/$DATE/g" template.rs > "$FILENAME"
 fi
+if [ ! -f "input/$DATE.ex" ]; then
+    echo "Enter example input:"
+    cat > "input/$DATE.ex"
+fi
 if [ ! -f "input/$DATE" ]; then
     echo "Enter input:"
     cat > "input/$DATE"
--- 2023/template.rs
+++ 2023/template.rs
@@ -1,13 +1,18 @@
-fn part1(input: String) -> String {
+use std::fmt::Display;
+
+fn part1(input: String) -> impl Display {
     todo!()
 }
 
-fn part2(input: String) -> String {
+fn part2(input: String) -> impl Display {
     todo!()
 }
 
 fn main() {
+    let example: String = include_str!("../../input/DAYNUM.ex").to_string();
     let input: String = include_str!("../../input/DAYNUM").to_string();
+    println!("EXAMPLE 1: {}", part1(example.clone()));
     println!("PART 1: {}", part1(input.clone()));
+    println!("EXAMPLE 2: {}", part2(example));
     println!("PART 2: {}", part2(input));
 }
