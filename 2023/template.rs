c3f92381023b8cea303baa2369562782238ffd4d
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2023-12-18 22:01:22 +0200

    2023: Fix template

--- 2023/template.rs
+++ 2023/template.rs
@@ -1,11 +1,13 @@
 use std::fmt::Display;
 
 fn part1(input: String) -> impl Display {
-    todo!()
+    todo!();
+    0
 }
 
 fn part2(input: String) -> impl Display {
-    todo!()
+    todo!();
+    0
 }
 
 fn main() {
@@ -16,7 +18,7 @@ fn main() {
         println!("EXAMPLE 2: {}", part1(example));
     } else {
         let input: String = include_str!("../../input/DAYNUM").to_string();
-        println!("PART 1: {}", solution(input.clone()));
-        println!("PART 2: {}", solution(input));
+        println!("PART 1: {}", part1(input.clone()));
+        println!("PART 2: {}", part1(input));
     }
 }
